500f35a0d79cf9de8c1949352f5e83b2f8c2287e
francisco.gallegos.luque <franciscogallegosluque@gmail.com>
2025-02-25 10:32:54 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Francisco Manuel Gallegos Luque on 25/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BucketListTests/BucketListTests.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListTests.swift
+//  BucketListTests
+//
+//  Created by Francisco Manuel Gallegos Luque on 25/02/2025.
+//
+
+import Testing
+@testable import BucketList
+
+struct BucketListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
